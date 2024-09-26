08f56067ca8fa9ff541b5b2f31a4007d001e34ba
davidleond99 <84214167+davidleond99@users.noreply.github.com>
2024-09-26 17:48:45 -0400

    Create Location & Weather Models

---
+++ packages/open_meteo_api/lib/src/models/weather.dart
@@ -0,0 +1,13 @@
+// {
+//   "current_weather": {
+//     "temperature": 15.3,
+//     "weathercode": 63
+//   }
+// }
+
+class Weather {
+  const Weather({required this.temperature, required this.weatherCode});
+
+  final double temperature;
+  final double weatherCode;
+}
