5de5424fc6c9b6319107ff47e5f5fc5f2aa08681
davidleond99 <84214167+davidleond99@users.noreply.github.com>
2024-09-26 22:19:16 -0400

    Create Business Logic Layer

---
+++ lib/modules/weather/presentation/view/weather_page.dart
@@ -0,0 +1,53 @@
+import 'package:clima_city/modules/search/presentation/view/search_page.dart';
+import 'package:clima_city/modules/settings/presentation/view/settings_page.dart';
+import 'package:clima_city/modules/weather/cubit/weather_cubit.dart';
+import 'package:clima_city/modules/weather/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class WeatherPage extends StatelessWidget {
+  const WeatherPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () => Navigator.of(context).push<void>(
+              SettingsPage.route(),
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: BlocBuilder<WeatherCubit, WeatherState>(
+          builder: (context, state) {
+            return switch (state.status) {
+              WeatherStatus.initial => const WeatherEmpty(),
+              WeatherStatus.loading => const WeatherLoading(),
+              WeatherStatus.failure => const WeatherError(),
+              WeatherStatus.success => WeatherPopulated(
+                  weather: state.weather,
+                  units: state.temperatureUnits,
+                  onRefresh: () {
+                    return context.read<WeatherCubit>().refreshWeather();
+                  },
+                ),
+            };
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.search, semanticLabel: 'Search'),
+        onPressed: () async {
+          final city = await Navigator.of(context).push(SearchPage.route());
+          if (!context.mounted) return;
+          await context.read<WeatherCubit>().fetchWeather(city);
+        },
+      ),
+    );
+  }
+}
